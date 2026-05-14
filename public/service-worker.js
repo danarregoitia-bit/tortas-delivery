e2fee913c0b2f3e73bca35d7480c1072064b91aa
Dan <dan.arregoitia@gmail.com>
2026-05-14 09:17:34 -0600

    Agregar PWA - App instalable

---
+++ public/service-worker.js
@@ -0,0 +1,38 @@
+// Service Worker básico para PWA
+const CACHE_NAME = 'tortas-ahogadas-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Instalación
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activación
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch - Network first, fallback to cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
